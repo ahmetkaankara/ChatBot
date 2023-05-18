9a277707674d5685795ca32641d7049024bc5fdd
ahmetkaankara <ahmetkaankara1907@gmail.com>
2023-05-18 17:12:32 +0300

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Ahmet Kaan Kara on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
